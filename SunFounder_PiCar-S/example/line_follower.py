7dc3873a2ace0acdae029930a219500022dfefe3
Raphael Bouchard <bour0703@usherbrooke.ca>
2024-10-27 14:54:52 -0400

    Augment speed value

--- SunFounder_PiCar-S/example/line_follower.py
+++ SunFounder_PiCar-S/example/line_follower.py
@@ -22,8 +22,8 @@ picar.setup()
 REFERENCES = [200, 200, 200, 200, 200]
 calibrate = True
 #calibrate = False
-forward_speed = 20
-backward_speed = 20
+forward_speed = 30
+backward_speed = 30
 turning_angle = 40
 
 max_off_track_count = 40
