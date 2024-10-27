08622b80404f97eae39858655b4d5f03f7bf2f36
Raphael Bouchard <bour0703@usherbrooke.ca>
2024-10-27 14:20:39 -0400

    Add diff calculation for calibration

--- SunFounder_PiCar-S/example/test_line_module.py
+++ SunFounder_PiCar-S/example/test_line_module.py
@@ -2,9 +2,15 @@ from SunFounder_Line_Follower import Line_Follower
 import time
 
 lf = Line_Follower.Line_Follower()
+
+previous = lf.read_analog()
 while True:
-	print(lf.read_analog())
+	current = lf.read_analog()
+	print("Previous : " + previous)
+	print("Current : " + current)
+	print("Diff : " + [current[i] - previous[i] for i in range(len(current))])
 	print(lf.read_digital())
 	print('')
+	previous = current
 	time.sleep(0.5)
 
