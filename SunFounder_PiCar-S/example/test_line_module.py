b47701cdc1eb446f34ed5ff852e603326abcaf8f
Raphael Bouchard <bour0703@usherbrooke.ca>
2024-10-27 14:22:55 -0400

    Fix error in test_module_line

--- SunFounder_PiCar-S/example/test_line_module.py
+++ SunFounder_PiCar-S/example/test_line_module.py
@@ -6,9 +6,9 @@ lf = Line_Follower.Line_Follower()
 previous = lf.read_analog()
 while True:
 	current = lf.read_analog()
-	print("Previous : " + previous)
-	print("Current : " + current)
-	print("Diff : " + [current[i] - previous[i] for i in range(len(current))])
+	print("Previous : ", previous)
+	print("Current : ", current)
+	print("Diff : ", [current[i] - previous[i] for i in range(len(current))])
 	print(lf.read_digital())
 	print('')
 	previous = current
