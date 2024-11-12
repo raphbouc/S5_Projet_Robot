5c5133722fabe835b9b1e0b7f4b82656e375efcb
Raphael Bouchard <bour0703@bour0703.ca>
2024-11-12 12:02:57 -0500

    Refactor some shit again

--- SunFounder_PiCar_S/example/test_ultrasonic_module.py
+++ SunFounder_PiCar_S/example/test_ultrasonic_module.py
@@ -1,34 +1,26 @@
 from ultrasonic_module import Ultrasonic_Avoidance
 import time
-from filter import apply_fir_filter
 
 UA = Ultrasonic_Avoidance()
 threshold = 10
 
 
 
-def main(array):
-	
+def main():
 	distance = UA.get_distance()
-	array.append(distance)
-	filter = apply_fir_filter(array)
 	status = UA.less_than(threshold)
-	if filter[-1] != -1:
-		print('distance', filter[-1], 'cm')
+	if distance != -1:
+		print('distance', distance, 'cm')
 		time.sleep(0.2)
 	else:
 		print(False)
-	""" if status == 1:
+	if status == 1:
 		print("Less than %d" % threshold)
 	elif status == 0:
 		print("Over %d" % threshold)
 	else:
 		print("Read distance error.")
- """
+
 if __name__=='__main__':
-	array = []
-	for i in range(304): 
-		distance = UA.get_distance()
-		array.append(distance)
 	while True:
-		main(array)
+		main()
