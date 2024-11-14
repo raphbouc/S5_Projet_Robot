1f3701b43d74e660993265632e54304ef29e3787
Kevin <ronk2602@usherbrooke.ca>
2024-11-14 14:59:58 -0500

    ajout valeurs du capteurs us, message changer en json

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import asyncio
 from websockets import serve
 import SunFounder_PiCar_S.example.SunFounder_Line_Follower.Line_Follower as LF
+from SunFounder_PiCar_S.example.SunFounder_Ultrasonic_Avoidance.Ultrasonic_Avoidance import Ultrasonic_Avoidance
 import picar
+import json
+
 
 lf = LF.Line_Follower()  
 REFERENCES = [200, 200, 200, 200, 200]
 lf.references = REFERENCES
+UA = Ultrasonic_Avoidance.Ultrasonic_Avoidance(20)
+threshold = 10
+value_array = []
+
+def push_to_data_array(input, array, max_length):
+    if len(array) < max_length:
+        array.append(input)
+    else:
+        array.pop(0)
+        array.append(input)
+
+def median_input(array):
+    sorted_array = sorted(array)
+    n = len(sorted_array)
+    
+    # Calculer la médiane
+    if n % 2 == 0:  # Si le nombre d'éléments est pair
+        median = (sorted_array[n // 2 - 1] + sorted_array[n // 2]) / 2
+    else:  # Si le nombre d'éléments est impair
+        median = sorted_array[n // 2]
+        
+    return median
 
 async def calibrate():
     """Calibrate the line follower sensor."""
@@ -33,9 +58,20 @@ async def calibrate():
 async def send_status(websocket):
     """Send line follower status to Godot."""
     while True:
+        distance = UA.get_distance()
+        push_to_data_array(distance, value_array, 10)
+        us_output = median_input(value_array)
         lt_status_now = lf.read_digital()  # Read current sensor status
-        await websocket.send(str(lt_status_now))  # Send the status to Godot
-        print("Message envoyé : ", lt_status_now)
+        
+        message = {
+            "lt_status": lt_status_now,
+            "us_output": us_output
+        }
+
+        json_message = json.dumps(message)
+
+        await websocket.send(json_message)  # Send the status to Godot
+        print("Message envoyé : ", json_message)
         await asyncio.sleep(0.1)  # Wait 100ms before next read
 
 async def echo(websocket, path):
