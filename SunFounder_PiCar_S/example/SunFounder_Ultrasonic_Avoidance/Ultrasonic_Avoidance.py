62942d2235a6124828ce6c09f46db2d7480e9ba2
Raphael Bouchard <bour0703@bour0703.ca>
2024-11-25 01:15:19 -0500

    second test

--- SunFounder_PiCar_S/example/SunFounder_Ultrasonic_Avoidance/Ultrasonic_Avoidance.py
+++ SunFounder_PiCar_S/example/SunFounder_Ultrasonic_Avoidance/Ultrasonic_Avoidance.py
@@ -1,98 +1,74 @@
-#!/usr/bin/env python
-'''
-**********************************************************************
-* Filename    : Ultrasonic_Avoidance.py
-* Description : A module for SunFounder Ultrasonic Avoidance
-* Author      : Cavon
-* Brand       : SunFounder
-* E-mail      : service@sunfounder.com
-* Website     : www.sunfounder.com
-* Update      : Cavon    2016-09-26    New release
-**********************************************************************
-'''
 import time
 import RPi.GPIO as GPIO
 
 class Ultrasonic_Avoidance(object):
-	timeout = 0.005
+    timeout = 0.005
 
-	def __init__(self, channel):
-		self.channel = channel
-		GPIO.setmode(GPIO.BCM)
+    def __init__(self, channel):
+        self.channel = channel
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.channel, GPIO.OUT)
 
-	def distance(self):
-		pulse_end = 0
-		pulse_start = 0
-		GPIO.setup(self.channel,GPIO.OUT)
-		GPIO.output(self.channel, False)
-		time.sleep(0.01)
-		GPIO.output(self.channel, True)
-		time.sleep(0.00001)
-		GPIO.output(self.channel, False)
-		GPIO.setup(self.channel,GPIO.IN)
+    def distance(self):
+        # Shorter wait time between trigger signals
+        GPIO.output(self.channel, False)
+        time.sleep(0.01)
+        GPIO.output(self.channel, True)
+        time.sleep(0.00001)
+        GPIO.output(self.channel, False)
+        GPIO.setup(self.channel, GPIO.IN)
 
-		timeout_start = time.time()
-		while GPIO.input(self.channel)==0:
-			pulse_start = time.time()
-			if pulse_start - timeout_start > self.timeout:
-				return -1
-		while GPIO.input(self.channel)==1:
-			pulse_end = time.time()
-			if pulse_start - timeout_start > self.timeout:
-				return -1
+        timeout_start = time.time()
+        pulse_start, pulse_end = 0, 0
+        while GPIO.input(self.channel) == 0:
+            pulse_start = time.time()
+            if pulse_start - timeout_start > self.timeout:
+                return -1
+        while GPIO.input(self.channel) == 1:
+            pulse_end = time.time()
+            if pulse_end - timeout_start > self.timeout:
+                return -1
 
-		if pulse_start != 0 and pulse_end != 0:
-			pulse_duration = pulse_end - pulse_start
-			distance = pulse_duration * 100 * 343.0 /2
-			distance = int(distance)
-			#print('start = %s'%pulse_start,)
-			#print('end = %s'%pulse_end)
-			if distance >= 0:
-				return distance
-			else:
-				return -1
-		else :
-			#print('start = %s'%pulse_start,)
-			#print('end = %s'%pulse_end)
-			return -1
+        if pulse_start and pulse_end:
+            pulse_duration = pulse_end - pulse_start
+            distance = pulse_duration * 34300 / 2  # Speed of sound in cm/s
+            return int(distance)
+        else:
+            return -1
 
-	def get_distance(self, mount = 5):
-		sum = 0
-		for i in range(mount):
-			a = self.distance()
-			#print('    %s' % a)
-			sum += a
-		return int(sum/mount)
-	def less_than(self, alarm_gate):
-		dis = self.get_distance()
-		status = 0
-		if dis >=0 and dis <= alarm_gate:
-			status = 1
-		elif dis > alarm_gate:
-			status = 0
-		else:
-			status = -1
-		#print('distance =',dis)
-		#print('status =',status)
-		return status
+    def get_distance(self, mount=3):  # Reduced mount to 3
+        total_distance = 0
+        for _ in range(mount):
+            total_distance += self.distance()
+        return total_distance // mount
+
+    def less_than(self, alarm_gate):
+        dis = self.get_distance()
+        if dis >= 0 and dis <= alarm_gate:
+            return 1
+        elif dis > alarm_gate:
+            return 0
+        else:
+            return -1
 
 def test():
-	UA = Ultrasonic_Avoidance(17)
-	threshold = 10
-	while True:
-		distance = UA.get_distance()
-		status = UA.less_than(threshold)
-		if distance != -1:
-			print('distance', distance, 'cm')
-			time.sleep(0.2)
-		else:
-			print(False)
-		if status == 1:
-			print("Less than %d" % threshold)
-		elif status == 0:
-			print("Over %d" % threshold)
-		else:
-			print("Read distance error.")
+    UA = Ultrasonic_Avoidance(17)
+    threshold = 10
+    while True:
+        distance = UA.get_distance()
+        status = UA.less_than(threshold)
+        if distance != -1:
+            print(f'distance: {distance} cm')
+        else:
+            print('Read distance error.')
+
+        if status == 1:
+            print(f"Less than {threshold}")
+        elif status == 0:
+            print(f"Over {threshold}")
+        else:
+            print("Read distance error.")
+        time.sleep(0.1)  # Reduced sleep time to avoid excessive delays
 
 if __name__ == '__main__':
-	test()
+    test()
