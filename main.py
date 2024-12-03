e507b91a466ada0e493fd0e707fe2fcd0ab70f4c
Raphael Bouchard <bour0703@bour0703.ca>
2024-12-03 13:09:02 -0500

    Revert sleepyjoe

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ fw.turning_max = 45
 value_array = [-1, -1, -1, -1 , -1]  # Partagé
 us_output = -1  # Stocke la médiane calculée
 oldrotation = 90
-sleepyjoe = 0
 # Création d'un verrou pour synchroniser l'accès aux variables partagées
 value_array_lock = asyncio.Lock()
 us_output_lock = asyncio.Lock()
@@ -114,7 +113,7 @@ async def calibrate():
 
 async def send_status(websocket):
     """Envoie les données du suiveur de ligne et de la distance."""
-    global us_output, value_array, sleepyjoe
+    global us_output, value_array
     distance_state = 1
     startTime = None
     while True:
@@ -122,7 +121,6 @@ async def send_status(websocket):
 
         array_message = lt_status_now.copy()
         elapsed_time = 0
-        sleepyjoe = 0
         if startTime:
             elapsed_time = time.time() - startTime
             print(f"Elapsed time: {elapsed_time}")
@@ -136,40 +134,30 @@ async def send_status(websocket):
             if local_us_output < 33 and distance_state == 1:
                 distance_state = 2
                 print("In state 2")
-            elif local_us_output < 18 and distance_state == 2:
+            elif local_us_output < 13 and distance_state == 2:
                 distance_state = 3
-                print("in state 3")
-            elif local_us_output < 13 and distance_state == 3:
+                print("In state 3")
+            elif local_us_output > 27 and distance_state == 3:
                 distance_state = 4
-                print("In state 4")
-            elif distance_state == 4 and sleepyjoe == 0:
-                time.sleep(2)
-                sleepyjoe = 1
-            elif local_us_output > 23 and distance_state == 4:
-                distance_state = 5
-                print("in state 5")
-            elif local_us_output > 27 and distance_state == 5:
-                distance_state = 6
                 startTime = time.time()
                 elapsed_time = 0
-                print("First timer started in state 6")
-        if elapsed_time > 3.2 and distance_state == 6:
+                print("First timer started in state 4")
+        if elapsed_time > 4.25 and distance_state == 4:
+            distance_state = 5
+            elapsed_time = 0
+            startTime = time.time()
+            print("Second timer started in state 5")
+        elif elapsed_time > 3 and distance_state == 5:
+            distance_state = 6
+            elapsed_time = 0
+            startTime = time.time()
+            print("In state 6")
+        elif elapsed_time > 2 and distance_state == 6:
             distance_state = 7
-            elapsed_time = 0
-            startTime = time.time()
-            print("Second timer started in state 7")
-        elif elapsed_time > 2.25 and distance_state == 7:
-            distance_state = 8
-            elapsed_time = 0
-            startTime = time.time()
-            print("In state 8")
-        elif elapsed_time > 1.1 and distance_state == 8:
-            distance_state = 9
-            print("In state 9")
-        elif sum(lt_status_now) >= 1 and distance_state == 9:
+            print("In state 7")
+        elif sum(lt_status_now) >= 1 and distance_state == 7:
             distance_state = 1
             value_array = [-1, -1, -1, -1, -1]
-            sleepyjoe = 0
             print("Back to state 1")
 
         array_message.append(distance_state)
