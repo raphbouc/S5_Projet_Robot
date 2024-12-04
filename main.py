25c28509dde4fa1da5e3ee46f10a7fa5c30205e6
Raphael Bouchard <bour0703@bour0703.ca>
2024-12-04 11:48:13 -0500

    Test offset rotation 6

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def process_message(json_message):
     try:
         data = json.loads(json_message)
         speed = int(float(data.get("speed", 0)) * 300)
-        rotation = int(float(data.get("rotation", 0)) + 105)
+        rotation = int(float(data.get("rotation", 0)) + 95)
         rotation = max(55, min(rotation, 135))
         return speed, rotation
     except Exception as e:
